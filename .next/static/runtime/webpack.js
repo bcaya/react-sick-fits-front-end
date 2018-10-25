6ae13cc772d3ac50167415d8ce130666532754ca
Bobby Caya <bob.caya1@gmail.com>
2018-10-24 18:26:42 -0600

    add meta info

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2fe93cd7437a00bab568";
+/******/ 	var hotCurrentHash = "92b0024b0716a0c4b46d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
