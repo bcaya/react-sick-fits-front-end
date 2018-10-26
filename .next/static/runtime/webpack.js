12aa6628a4f65db3a385cc7b403f583b44ddb7ef
Bobby Caya <bob.caya1@gmail.com>
2018-10-25 22:54:33 -0600

    add _document for loading styled components

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "700a1c960c01cbde5466";
+/******/ 	var hotCurrentHash = "4935d30e846adb8d9532";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
