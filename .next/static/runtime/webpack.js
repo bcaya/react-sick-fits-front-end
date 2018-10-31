08015a749638739b68885c462a3eb133d1e6eec7
Bobby Caya <bob.caya1@gmail.com>
2018-10-30 21:56:25 -0600

    add styled components and map over data items

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e56f94639b1ec90dedab";
+/******/ 	var hotCurrentHash = "7dea881fecd158f8987a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
