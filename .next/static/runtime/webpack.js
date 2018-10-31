16b70429fa8068702b2ef7de556e825be62d1065
Bobby Caya <bob.caya1@gmail.com>
2018-10-31 17:59:23 -0600

    add price tag and title link

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7dea881fecd158f8987a";
+/******/ 	var hotCurrentHash = "14a1796d2a741245a6a3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
