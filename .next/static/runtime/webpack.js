c700fb936ac8b82a1f69830fe8633e85172d9f2a
Bobby Caya <bob.caya1@gmail.com>
2018-11-07 09:02:40 -0700

    add client side re rendering for items page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8b69fedc52d999ad36bc";
+/******/ 	var hotCurrentHash = "4003a39783430408595a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
