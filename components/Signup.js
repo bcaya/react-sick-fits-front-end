a6ac5403a0230d27eea1c5c60c40bdf2084006b5
Bobby Caya <bob.caya1@gmail.com>
2018-11-13 18:15:14 -0700

    change div into Column styled components

--- components/Signup.js
+++ components/Signup.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import {Mutation} from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
-import Error from './styles/Error';
+import Error from './ErrorMessage';
 
  class Signup extends Component {
   render() {
     return (
-      <div />
+    <Form>
+      <fieldset>
+        <h2>Sign Up for An Account</h2>
+      </fieldset>
+    </Form>
     )
   }
 }
