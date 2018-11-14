b9f09582dc6a168ae82b2a67f5444137f76f4e76
Bobby Caya <bob.caya1@gmail.com>
2018-11-13 18:08:32 -0700

    stub out signup component

---
+++ components/Signup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {Mutation} from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import Error from './styles/Error';
+
+ class Signup extends Component {
+  render() {
+    return (
+      <div />
+    )
+  }
+}
+
+export default Signup
---
+++ pages/signup.js
@@ -0,0 +1,6 @@
+const Signup = props => (
+  <div>
+    <p>Signup!</p>
+  </div>
+)
+export default Signup;
