a5dc39d9d2e24ab2cee79dd063185713079c3d25
Bobby Caya <bob.caya1@gmail.com>
2018-11-26 19:04:05 -0700

    add signout mutation to component

--- components/Signout.js
+++ components/Signout.js
@@ -3,6 +3,19 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
 
-const Signout = props => <button>Sign Out</button>;
+const SIGN_OUT_MUTATION = gql `
+  mutation SIGN_OUT_MUTATION {
+    signout {
+      message
+    }
+  }
+ `
+
+
+const Signout = props => (
+  <Mutation>
+    {signout => <button onClick={signout}>Sign Out</button>}
+  </Mutation>)
+  ;
 
 export default Signout
