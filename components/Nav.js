4745b9fae6481e04e03ba547a2c5ea65ea8a9b0f
Bobby Caya <bob.caya1@gmail.com>
2018-12-06 20:53:02 -0700

    nav bar changes

--- components/Nav.js
+++ components/Nav.js
@@ -3,29 +3,34 @@ import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
 const Nav = () => (
-  <NavStyles>
   <User>
-    {({data: { me } }) => {
-      if (me) return <p>{me.name}</p>
-      return null;
-    }}
+    {({ data: { me } }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+            <Signout />
+          </>
+        )}
+        {!me && (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
   </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
 )
 
 export default Nav;
