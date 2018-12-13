a988cdb7fa5182fa34d679a66541a24e9646f36b
Bobby Caya <bob.caya1@gmail.com>
2018-12-12 18:18:57 -0700

    start table for going through array of users and there access

--- components/Permissions.js
+++ components/Permissions.js
@@ -1,13 +1,37 @@
 import { Query } from 'react-apollo';
 import Error from './ErrorMessage';
+import gql from 'graphql-tag';
+import Table from './styles/Table'; 
 
+const ALL_USERS_QUERY = gql `
+  query{ 
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`
 
 const Permissions = (props) => (
   <Query query={ALL_USERS_QUERY}>
-    {({data, loading, error}) => (
+    {({data, loading, error}) => console.log(data) || (
       <div>
       <Error error={error} />
-        <p>Hey</p>
+        <div>
+          <h2>Manage Permissions</h2>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>{data.users.map(user => user.name)} 
+            </tbody>
+          </Table>
+        </div>
       </div>
     )}
   </Query>
