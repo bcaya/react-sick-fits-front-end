55809d31c3a57a540870f48cdfd21a203f5ae936
Bobby Caya <bob.caya1@gmail.com>
2019-01-18 14:34:43 -0700

    complete local state linking to apollo and toggle cart

--- components/Cart.js
+++ components/Cart.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import CartStyles from './styles/CartStyles';
+import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
 import Supreme from './styles/Supreme';
 import SickButton from './styles/SickButton';
 import CloseButton from './styles/CloseButton';
 
-const Cart = () => {
-  return <CartStyles open>
-    <header>
-      <CloseButton title="close">&times;</CloseButton>
-      <Supreme>Your Cart</Supreme>
-      <p>You Have __ Items in your cart.</p>
-    </header>
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
 
-    <footer>
-      <p>$10.10</p>
-      <SickButton>Checkout</SickButton>
-    </footer>
-  </CartStyles>
-}
+const TOGGLE_CART_MUTATION = gql `
+  mutation {
+    toggleCart @client
+  }
+`
+const Cart = () => (
+  <Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
+
+  <Query query={LOCAL_STATE_QUERY}>
+    {({data}) => 
+      console.log(data) || (
+    <CartStyles open={data.cartOpen}>
+      <header>
+        <CloseButton title="open" onClick={toggleCart}>&times;</CloseButton>
+        <Supreme>Your Cart</Supreme>
+        <p>You Have __ Items in your cart.</p>
+      </header>
+  
+      <footer>
+        <p>$10.10</p>
+        <SickButton>Checkout</SickButton>
+      </footer>
+    </CartStyles>
+  )}
+  </Query>
+  )}
+</Mutation>
+)
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
