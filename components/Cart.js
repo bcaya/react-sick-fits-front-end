ffdab0dbcf4b3d95141b8b968d80c3d1238017c7
Bobby Caya <bob.caya1@gmail.com>
2019-01-18 14:55:40 -0700

    displaying cart on screen in progress

--- components/Cart.js
+++ components/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CartStyles from './styles/CartStyles';
 import gql from 'graphql-tag';
+import User from './User';
 import { Query, Mutation } from 'react-apollo';
 import Supreme from './styles/Supreme';
 import SickButton from './styles/SickButton';
@@ -18,27 +19,36 @@ const TOGGLE_CART_MUTATION = gql `
   }
 `
 const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
+  <User>{({ data: {me}}) => {
+    if(!me) return null;
+    console.log(me);
+    return(
+      <Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
 
-  <Query query={LOCAL_STATE_QUERY}>
-    {({data}) => 
-      console.log(data) || (
-    <CartStyles open={data.cartOpen}>
-      <header>
-        <CloseButton title="open" onClick={toggleCart}>&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You Have __ Items in your cart.</p>
-      </header>
-  
-      <footer>
-        <p>$10.10</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyles>
-  )}
-  </Query>
-  )}
-</Mutation>
+      <Query query={LOCAL_STATE_QUERY}>
+        {({data}) => 
+          console.log(data) || (
+            <CartStyles open={data.cartOpen}>
+              <header>
+                <CloseButton title="open" onClick={toggleCart}>&times;</CloseButton>
+                <Supreme>{me.name}'s Cart</Supreme>
+                <p>You Have {me.cart.length} Item
+                {me.cart.length === 1 ? '' : 's'} in your cart.</p>
+              </header>
+              <ul>
+                {me.cart.map(cartItem => <li>{cartItem.id}</li>)}
+              </ul>
+              <footer>
+                <p>$10.10</p>
+                <SickButton>Checkout</SickButton>
+              </footer>
+            </CartStyles>
+          )}
+          </Query>
+          )}
+        </Mutation>
+    )
+  }}</User>
 )
 
 export default Cart;
