9568b614666a839b1faae511d60ca9fe9e0c56c7
Bobby Caya <bob.caya1@gmail.com>
2019-01-21 19:11:19 -0700

    update nav with counter

--- components/Nav.js
+++ components/Nav.js
@@ -4,6 +4,8 @@ import {TOGGLE_CART_MUTATION} from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
+
 const Nav = () => (
   <User>
     {({ data: { me } }) => (
@@ -26,7 +28,10 @@ const Nav = () => (
             <Mutation mutation={TOGGLE_CART_MUTATION}>
               {(toggleCart) => (
 
-            <button onClick={toggleCart}>My Cart</button>
+            <button onClick={toggleCart}>
+              My Cart
+              <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}></CartCount>
+            </button>
               )}
             </Mutation>
           </>
