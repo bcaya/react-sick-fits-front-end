50bac915f96fc12a5f9dd935c1e79492f2c70278
Bobby Caya <bob.caya1@gmail.com>
2019-01-22 19:20:05 -0700

    Set up search items query with onChange function

--- components/Search.js
+++ components/Search.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import Downshift from 'downshift';
 import Router from 'next/router';
-import { ApolloConsumer } from 'react-adopt';
+import { ApolloConsumer } from 'react-apollo';
 import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
 import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
 
+const SEARCH_ITEMS_QUERY = gql `
+  query SEARCH_ITEMS_QUERY($searchTerm: String!){
+    items(where: { OR: [{ title_contains: $searchTerm }, {description_contains: $searchTerm }]}){
+      id
+      image
+      title
+    }
+  }
+`
+
 class AutoComplete extends React.Component {
+  
+   onChange = async (e, client) => {
+     // Manually query apollo client 
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { searchTerm: e.target.value},
+    });
+    console.log(res);
+  }
   render(){
     return(
     <SearchStyles>
       <div>
-        <input type="search" />
+      <ApolloConsumer>
+        {client => (<input type="search" onChange={e => {e.persist(); this.onChange(e, client);}}/>
+        )}
+      </ApolloConsumer>
         <DropDown>
           <p>Items will go here</p>
         </DropDown>
       </div>
-    </SearchStyles>)
+    </SearchStyles>
+    )
   }
 }
 
