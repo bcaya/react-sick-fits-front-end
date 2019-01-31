497a53e0198489964d4e87535067a62c80f3c75f
Bobby Caya <bob.caya1@gmail.com>
2019-01-30 19:54:05 -0700

    set up shallow rendering testing for item page

---
+++ __tests__/mocking.test.js
@@ -0,0 +1,12 @@
+
+
+describe('mocking learning', () => {
+  it('mocks a reg function', () => {
+    const fetchDogs = jest.fn();
+    fetchDogs('snickers');
+    expect(fetchDogs).toHaveBeenCalled();
+    expect(fetchDogs).toHaveBeenCalledWith('snickers');
+    fetchDogs('hugo');
+    expect(fetchDogs).toHaveBeenCalledTimes(2);
+  })
+})
